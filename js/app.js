e2312e6f29c0febd03943964acd394b86a1b4b44
Zarkyoz <alexandre.nickles@gmail.com>
2021-01-19 20:49:46 +0100

    delete scroll & add ^ to axe button

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
         //scrolling de l'header
         
-        let scrolleur = document.getElementById('scroll')
-        let posScrolleur = 0
-        scrolleur.children[1].style.transform = "rotate(90deg) scale(0.5, 3) translate(-50%,5%)"
-        window.onload = function(){
-            setTimeout(() => {
-                if(window.scrollY < window.innerHeight * 0.5){
-                    scrolleur.style.display = "flex"
-                    posScrolleur = 1                   
-                }
-                scrolleur.style.top = (window.innerHeight - (scrolleur.offsetHeight * 1.5)) + 'px' 
-            }, 3000);
+        // let scrolleur = document.getElementById('scroll')
+        // let posScrolleur = 0
+        // scrolleur.children[1].style.transform = "rotate(90deg) scale(0.5, 3) translate(-50%,5%)"
+        // window.onload = function(){
+        //     setTimeout(() => {
+        //         if(window.scrollY < window.innerHeight * 0.5){
+        //             scrolleur.style.display = "flex"
+        //             posScrolleur = 1                   
+        //         }
+        //         scrolleur.style.top = (window.innerHeight - (scrolleur.offsetHeight * 1.5)) + 'px' 
+        //     }, 3000);
 
-        }
+        // }
         
-        window.addEventListener('scroll', ()=>{
-            if(posScrolleur == 1 && (window.scrollY > window.innerHeight * 0.5)){
-                console.log('chien')
-                scrolleur.style.display = "none"
-                posScrolleur = 0
-            }
-        })
+        // window.addEventListener('scroll', ()=>{
+        //     if(posScrolleur == 1 && (window.scrollY > window.innerHeight * 0.5)){
+        //         scrolleur.style.display = "none"
+        //         posScrolleur = 0
+        //     }
+        // })
